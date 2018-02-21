93d53236697113b1d9a73d7894129c9958059fe0
Boris Tseytlin <b.tseytlin@start.film>
2018-02-21 17:03:33 +0300

    fix typos

--- celery_dao/__main__.py
+++ celery_dao/__main__.py
@@ -4,12 +4,12 @@ from .tasks import add
 
 def main():
     print('Running app')
-    print('I will wait 3 seconds for celery to connect to brocker')
+    print('I will wait 3 seconds for celery to connect to broker')
     time.sleep(3)
     print('I will now call a task')
     time.sleep(10)
     result = add.apply_async((2, 2,))
-    print('Task called, waiting for celery to give us the answer')
+    print('Task called, waiting for celery to give me the answer')
     print(f'Answer: {result.get()}')
 
 if __name__ == '__main__':
